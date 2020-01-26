6568c90e2e7d69a8fff5885e5bd5197b49782735
Satyo Wicaksana <satyowicaksana@gmail.com>
2020-01-26 12:20:17 +0700

    Updates

---
+++ precache-manifest.f606b99cca2c2e67ca10d53b1a850321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45a844ae7c1a9ab3fd1801c653a8dde",
+    "url": "/furniture-finder/index.html"
+  },
+  {
+    "revision": "fa05844397f879b5e764",
+    "url": "/furniture-finder/static/css/main.46ae75c0.chunk.css"
+  },
+  {
+    "revision": "2afd89d193c32366c62b",
+    "url": "/furniture-finder/static/js/2.ed72d90f.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/furniture-finder/static/js/2.ed72d90f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa05844397f879b5e764",
+    "url": "/furniture-finder/static/js/main.d7506f09.chunk.js"
+  },
+  {
+    "revision": "053d0ca55247a158a84f",
+    "url": "/furniture-finder/static/js/runtime-main.006d80cd.js"
+  }
+]);
